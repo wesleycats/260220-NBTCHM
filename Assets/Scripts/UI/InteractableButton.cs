0f6a8d3c8936ea1dbcd450fb55ddf79af192e86e
developerdavebakker <davebakkerdev@gmail.com>
2020-03-11 12:21:49 +0100

    Player Setup (v1.1.2)
    Added
    - Interactable button script for the user interface
    - Player folders

---
+++ Assets/Scripts/UI/InteractableButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableButton : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // Testing purpose comment
+}
