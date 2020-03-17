ce028ee6291f50b3434b1528efc6f183e154c387
gwnrobin <robinschellingerhout@live.nl>
2020-03-17 15:57:58 +0100

    UI Information pages (1.4.1)
    - Added a easy way to add location infomation pages
    - Added a Font

---
+++ Assets/Scripts/LocationPageData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "LocationData", menuName = "Data", order = 1)]
+public class LocationPageData : ScriptableObject
+{
+    public string LocationName;
+
+    public List<Sprite> Pictures;
+
+    public List<Review> Review;
+    [TextArea(4, 3)]
+    public string Information;
+}
+
+[System.Serializable]
+public struct Review
+{
+    public string Name;
+    public int Stars;
+    [TextArea(4, 3)]
+    public string Comment;
+}
